3eacfa6eb5234305fa5b358be7f9b743b6c96c59
mayan <mayan31370@126.com>
2019-03-19 19:12:21 +0800

    make it run with openjdk

--- src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
+++ src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
@@ -1,13 +1,9 @@
 package io.tradingchain.peppa.util;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.springframework.util.Base64Utils;
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -16,54 +12,13 @@ public class AESEncryptionUtil {
 
     public static String encrypt(String seed, byte[] original) {
         try {
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(seed.getBytes());
-
-            KeyGenerator generator = KeyGenerator.getInstance("AES");
-            generator.init(128, random);
-
-            SecretKey key = new SecretKeySpec(generator.generateKey().getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            byte[] encrypted = cipher.doFinal(original);
-            return new BASE64Encoder().encode(encrypted);
+            byte[] encrypted = getCipher(seed, Cipher.ENCRYPT_MODE).doFinal(original);
+            return Base64Utils.encodeToString(encrypted);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static byte[] decrypt(String seed, InputStream encrypted) {
-        try {
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(seed.getBytes());
-
-            KeyGenerator generator = KeyGenerator.getInstance("AES");
-            generator.init(128, random);
-
-            SecretKey key = new SecretKeySpec(generator.generateKey().getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-
-            return cipher.doFinal(new BASE64Decoder().decodeBuffer(encrypted));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
@@ -75,25 +30,12 @@ public class AESEncryptionUtil {
 
     public static byte[] decrypt(String seed, byte[] encrypted) {
         try {
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(seed.getBytes());
-
-            KeyGenerator generator = KeyGenerator.getInstance("AES");
-            generator.init(128, random);
-
-            SecretKey key = new SecretKeySpec(generator.generateKey().getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-
-            return cipher.doFinal(new BASE64Decoder().decodeBuffer(new String(encrypted, StandardCharsets.UTF_8)));
+            return getCipher(seed, Cipher.DECRYPT_MODE).doFinal(Base64Utils.decode(encrypted));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
@@ -101,5 +43,16 @@ public class AESEncryptionUtil {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private static Cipher getCipher(String seed, int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        random.setSeed(seed.getBytes());
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(128, random);
+        SecretKey key = new SecretKeySpec(generator.generateKey().getEncoded(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(encryptMode, key);
+        return cipher;
     }
 }
