4368536c4d8b1ba591c928f4fc3eb9c46fe5f410
zhangpeng <zhangpeng@chenyuzhifu.com>
2019-06-04 16:41:15 +0800

    修改加密方式，更新kline添加

--- src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
+++ src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
@@ -1,56 +1,62 @@
 package io.tradingchain.peppa.util;
 
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import org.apache.commons.codec.binary.Base64;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 public class AESEncryptionUtil {
+    /**
+     * 加密用的Key 可以用26个字母和数字组成
+     * 此处使用AES-128-CBC加密模式，key需要为16位。
+     */
+    private static String ivParameter = "1234567890123456";
 
-    public static String encrypt(String seed, byte[] original) {
+    // 加密
+    public static String encrypt(String sKey,byte[] sSrc) {
+        Cipher cipher = null;
         try {
-            return new String(Base64.encodeBase64(getCipher(seed, Cipher.ENCRYPT_MODE).doFinal(original)));
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] raw = sKey.getBytes();
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            byte[] encrypted = cipher.doFinal(sSrc);
+            return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码。
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
             e.printStackTrace();
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
-        } catch (BadPaddingException e) {
+        } catch (InvalidAlgorithmParameterException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static byte[] decrypt(String seed, byte[] encrypted) {
+    // 解密
+    public static byte[] decrypt(String sKey,byte[] sSrc) {
         try {
-            return getCipher(seed, Cipher.DECRYPT_MODE).doFinal(Base64.decodeBase64(encrypted));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
+            byte[] raw = sKey.getBytes("ASCII");
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(new String(sSrc));//先用base64解密
+            byte[] original = cipher.doFinal(encrypted1);
+            //String originalString = new String(original, "utf-8");
+            return original;
+        } catch (Exception ex) {
+            return null;
         }
-        return null;
-    }
-
-    private static Cipher getCipher(String seed, int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-        random.setSeed(seed.getBytes());
-        KeyGenerator generator = KeyGenerator.getInstance("AES");
-        generator.init(128, random);
-        SecretKey key = new SecretKeySpec(generator.generateKey().getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(encryptMode, key);
-        return cipher;
     }
 }
