52edc79d47bfe0a711486e286dc2684f2d8ec9d5
simon <simon@tcpay.info>
2019-10-29 15:36:07 +0800

    1.4.1 追加异步通知的md5加密，修改请求方法中参数名称

--- src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
+++ src/main/java/io/tradingchain/peppa/util/AESEncryptionUtil.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.TreeMap;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -42,6 +45,53 @@ public class AESEncryptionUtil {
       return getCipher(sKey, ivParameter, Cipher.DECRYPT_MODE)
           .doFinal(new BASE64Decoder().decodeBuffer(new String(sSrc)));
   }
+
+  /**
+   * 签名前字符串
+   * @param data
+   * @param secret
+   * @return
+   */
+  public static final String getOTCPreSignStr(Map data, String secret) {
+    Map<String, String> treeMap = new TreeMap<>(data);
+    StringBuilder sb = new StringBuilder();
+    treeMap.forEach((k, v) -> {
+      if (!"sign".equals(k) && null != v) {
+        sb.append("&" + k + "=" + v);
+      }
+    });
+    sb.append("&key=" + secret);
+    return sb.toString().substring(1);
+  }
+  /***
+   * md5 加密
+   */
+  public final static String MD5OTC(String s) {
+    char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    try {
+      byte[] btInput = s.getBytes("UTF-8");
+      // 获得MD5摘要算法的 MessageDigest 对象
+      MessageDigest mdInst = MessageDigest.getInstance("MD5");
+      // 使用指定的字节更新摘要
+      mdInst.update(btInput);
+      // 获得密文
+      byte[] md = mdInst.digest();
+      // 把密文转换成十六进制的字符串形式
+      int j = md.length;
+      char str[] = new char[j * 2];
+      int k = 0;
+      for (int i = 0; i < j; i++) {
+        byte byte0 = md[i];
+        str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+        str[k++] = hexDigits[byte0 & 0xf];
+      }
+      return new String(str);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
 
   /**
    * 加密算法
