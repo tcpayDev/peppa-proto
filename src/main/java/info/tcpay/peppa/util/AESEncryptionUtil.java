b4abaea866c5e9c58ae248669ead663347023472
my <my@tcpay.info>
2020-03-06 12:56:30 +0800

    use github actions and packages instead of travis and jitpack

--- src/main/java/info/tcpay/peppa/util/AESEncryptionUtil.java
+++ src/main/java/info/tcpay/peppa/util/AESEncryptionUtil.java
@@ -1,7 +1,7 @@
-package io.tradingchain.peppa.util;
+package info.tcpay.peppa.util;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
@@ -19,58 +19,49 @@ import sun.misc.BASE64Encoder;
 
 public class AESEncryptionUtil {
 
+  private static final char[] HEX_DIGITS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8',
+      '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
   /**
-   * 加密用的Key 可以用26个字母和数字组成 此处使用AES-128-CBC加密模式，key需要为16位。
-   * 偏移量必须为16位
-   * @param sKey
-   * @param ivParameter
-   * @param sSrc
-   * @return
+   * 加密用的Key 可以用26个字母和数字组成 此处使用AES-128-CBC加密模式，key需要为16位。 偏移量必须为16位
    */
   public static String encrypt(String sKey, String ivParameter, byte[] sSrc)
-      throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
-      return new BASE64Encoder()
-          .encode(getCipher(sKey, ivParameter, Cipher.ENCRYPT_MODE).doFinal(sSrc));//此处使用BASE64做转码。
+      throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+    return new BASE64Encoder()
+        .encode(getCipher(sKey, ivParameter, Cipher.ENCRYPT_MODE).doFinal(sSrc));//此处使用BASE64做转码。
   }
 
 
   /**
    * 解密
-   * @param sKey key
-   * @param ivParameter 偏移量
-   * @param sSrc
-   * @return
    */
   public static byte[] decrypt(String sKey, String ivParameter, byte[] sSrc)
       throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
-      return getCipher(sKey, ivParameter, Cipher.DECRYPT_MODE)
-          .doFinal(new BASE64Decoder().decodeBuffer(new String(sSrc)));
+    return getCipher(sKey, ivParameter, Cipher.DECRYPT_MODE)
+        .doFinal(new BASE64Decoder().decodeBuffer(new String(sSrc)));
   }
 
   /**
    * 签名前字符串
-   * @param data
-   * @param secret
-   * @return
    */
-  public static final String getOTCPreSignStr(Map data, String secret) {
+  public static String getOTCPreSignStr(Map<String, String> data, String secret) {
     Map<String, String> treeMap = new TreeMap<>(data);
-    StringBuilder sb = new StringBuilder();
+    StringBuffer sb = new StringBuffer();
     treeMap.forEach((k, v) -> {
       if (!"sign".equals(k) && null != v) {
-        sb.append("&" + k + "=" + v);
+        sb.append('&').append(k).append('=').append(v);
       }
     });
-    sb.append("&key=" + secret);
+    sb.append("&key=").append(secret);
     return sb.toString().substring(1);
   }
+
   /***
    * md5 加密
    */
-  public final static String MD5OTC(String s) {
-    char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+  public static String MD5OTC(String s) {
     try {
-      byte[] btInput = s.getBytes("UTF-8");
+      byte[] btInput = s.getBytes(StandardCharsets.UTF_8);
       // 获得MD5摘要算法的 MessageDigest 对象
       MessageDigest mdInst = MessageDigest.getInstance("MD5");
       // 使用指定的字节更新摘要
@@ -79,12 +70,11 @@ public class AESEncryptionUtil {
       byte[] md = mdInst.digest();
       // 把密文转换成十六进制的字符串形式
       int j = md.length;
-      char str[] = new char[j * 2];
+      char[] str = new char[j * 2];
       int k = 0;
-      for (int i = 0; i < j; i++) {
-        byte byte0 = md[i];
-        str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-        str[k++] = hexDigits[byte0 & 0xf];
+      for (byte byte0 : md) {
+        str[k++] = HEX_DIGITS[byte0 >>> 4 & 0xf];
+        str[k++] = HEX_DIGITS[byte0 & 0xf];
       }
       return new String(str);
     } catch (Exception e) {
@@ -93,22 +83,12 @@ public class AESEncryptionUtil {
     }
   }
 
-
   /**
    * 加密算法
-   * @param seed key
-   * @param ivParameter 偏移量
-   * @param encryptMode 模式
-   * @return
-   * @throws UnsupportedEncodingException
-   * @throws NoSuchPaddingException
-   * @throws NoSuchAlgorithmException
-   * @throws InvalidAlgorithmParameterException
-   * @throws InvalidKeyException
    */
   private static Cipher getCipher(String seed, String ivParameter, int encryptMode)
-      throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
-    byte[] raw = seed.getBytes("ASCII");
+      throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
+    byte[] raw = seed.getBytes(StandardCharsets.US_ASCII);
     SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
     IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
