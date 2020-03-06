a8edc80fa1c4e212d169eb02fc662914733304fe
my <my@tcpay.info>
2020-03-06 13:16:29 +0800

    make it clean

--- src/main/java/info/tcpay/peppa/util/AESEncryptionUtil.java
+++ src/main/java/info/tcpay/peppa/util/AESEncryptionUtil.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Map;
-import java.util.TreeMap;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -18,9 +15,6 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 public class AESEncryptionUtil {
-
-  private static final char[] HEX_DIGITS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8',
-      '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
   /**
    * 加密用的Key 可以用26个字母和数字组成 此处使用AES-128-CBC加密模式，key需要为16位。 偏移量必须为16位
@@ -39,48 +33,6 @@ public class AESEncryptionUtil {
       throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
     return getCipher(sKey, ivParameter, Cipher.DECRYPT_MODE)
         .doFinal(new BASE64Decoder().decodeBuffer(new String(sSrc)));
-  }
-
-  /**
-   * 签名前字符串
-   */
-  public static String getOTCPreSignStr(Map<String, String> data, String secret) {
-    Map<String, String> treeMap = new TreeMap<>(data);
-    StringBuffer sb = new StringBuffer();
-    treeMap.forEach((k, v) -> {
-      if (!"sign".equals(k) && null != v) {
-        sb.append('&').append(k).append('=').append(v);
-      }
-    });
-    sb.append("&key=").append(secret);
-    return sb.toString().substring(1);
-  }
-
-  /***
-   * md5 加密
-   */
-  public static String MD5OTC(String s) {
-    try {
-      byte[] btInput = s.getBytes(StandardCharsets.UTF_8);
-      // 获得MD5摘要算法的 MessageDigest 对象
-      MessageDigest mdInst = MessageDigest.getInstance("MD5");
-      // 使用指定的字节更新摘要
-      mdInst.update(btInput);
-      // 获得密文
-      byte[] md = mdInst.digest();
-      // 把密文转换成十六进制的字符串形式
-      int j = md.length;
-      char[] str = new char[j * 2];
-      int k = 0;
-      for (byte byte0 : md) {
-        str[k++] = HEX_DIGITS[byte0 >>> 4 & 0xf];
-        str[k++] = HEX_DIGITS[byte0 & 0xf];
-      }
-      return new String(str);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
   }
 
   /**
